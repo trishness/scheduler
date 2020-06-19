8db5e5cb3e919f862ea65ad025ccb36c8780025d
Trish Ness <trishnss@gmail.com>
2020-06-18 18:51:21 -0700

    color loop added, some bugs. button function started, incomplete

--- script.js
+++ script.js
@@ -1,44 +1,66 @@
 var d = new Date();
 var strDate = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").append(strDate);
-textArea1=$("#9am")
-textArea2=$("#10am")
-textArea3=$("#11am")
-textArea4=$("#12pm")
-textArea5=$("#1pm")
-textArea6=$("#2pm")
-textArea7=$("#3pm")
-textArea8=$("#4pm")
-textArea9=$("#5pm")
 
+
+//textArea1=$("#9am")
+//textArea2=$("#10am")
+//textArea3=$("#11am")
+//textArea4=$("#12pm")
+//textArea5=$("#1pm")
+//textArea6=$("#2pm")
+//textArea7=$("#3pm")
+//textArea8=$("#4pm")
+//textArea9=$("#5pm")
+currentTime=moment().format("ha");
+console.log(currentTime);
 
 
 console.log(moment().format("ha"));
 
-$("#9")
-
 var textAreas = $(".col-8")
 console.log(textAreas)
 
+var buttons =$(".savBtn")
+console.log(buttons)
+
+
+//loop for checking time and displaying textArea color
 for (i=0; i<textAreas.length; i++) {
-     //if(textAreas[i].attributes[0].nodeValue === (moment().format("ha"))) {
+     
+     if(textAreas[i].attributes[0].nodeValue===currentTime) {
+          $(textAreas[i]).attr("class", "present");
+    };
+
+    if(textAreas[i].attributes[0].nodeValue > currentTime) {
+          $(textAreas[i]).attr("class", "past");
+     };
+
+     if(textAreas[i].attributes[0].nodeValue < currentTime) {
+          $(textAreas[i]).attr("class", "future");
+     };
+      //not sure why this makes my col-8 look like a col-2
+}
+
+
+//loop for saving textArea input when clicked
+$("button").click(function()){
+for (i=0; i<buttons.length; i++) {
+     if(buttons[i]){};
+}
+});
+     //if id of save button is clicked, save that text area's content to local storage
+
+
+
+//if(textAreas[i].attributes[0].nodeValue === (moment().format("ha"))) {
           //console.log("true")
           //(textArea+i).attr("class","past")
-     //}
-     if(textAreas[i].attributes[0].nodeValue===(moment().format("ha"))) {
-          textAreas[i].setAttribute("class", "present");
-    }
-
-    if(textAreas[i].attributes[0].nodeValue > (moment().format("ha"))) {
-          textAreas[i].setAttribute("class", "past");
-}
-
-    //that last one did respond to the current time, but made the block disappear//
-}
-
+     //} from underneath the loop with aslan
 
 
 //if current time < block time, color = green
 //if current time = block time, color = red
 //if current time > block tim, coloe = grey
 
+//to save to local storage... localStorage.setItem("todo", textArea[i].textContent)
